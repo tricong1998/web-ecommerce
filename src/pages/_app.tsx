d234297050d67253d8ed630ade6cd6c61dd228e1
Conan <conan.congnguyen@setel.com>
2023-02-27 11:48:38 +0700

    feat: config i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '@/styles/globals.css';
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import '../../i18next.config';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
